e29797dcf6151b7ebec1f60ac1fdfe0f4132f5b7
Alex Nordin <alex_m_nordin@pm.me>
2024-03-14 20:17:03 +0100

    Add stm dev shell

--- flake.nix
+++ flake.nix
@@ -8,6 +8,10 @@
         path = ./rust-dev-template;
         description = "A template for Rust development, with devenv and flake-parts";
       };
+      stm-c-dev = {
+        path = ./stm-c-template;
+        description = "A template for a C dev-env for stm32f3, with code generation and flashing";
+      };
     };
   };
 }
