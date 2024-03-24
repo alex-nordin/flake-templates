f263cb28daba8a53e3047b3aa540ec6ccd273aac
Alex Nordin <alex_m_nordin@pm.me>
2024-03-24 01:54:32 +0100

    Really fix path typo

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
         description = "A template for a C dev-env for stm32f3, with code generation and flashing";
       };
       c-meson = {
-        path = ./c_meson;
+        path = ./c-meson;
         description = "A template for C++ projects using meson to build";
       };
     };
