4061526d200579eea95058bf8670f27f1620ddbe
Alex Nordin <alex_m_nordin@pm.me>
2024-03-24 01:59:42 +0100

    Change template name

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
         path = ./stm-c-template;
         description = "A template for a C dev-env for stm32f3, with code generation and flashing";
       };
-      c-meson = {
+      c-meson-dev = {
         path = ./c-meson;
         description = "A template for C++ projects using meson to build";
       };
